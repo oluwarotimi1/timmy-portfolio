1238072b181cf63f0521ee33a37c60dffd9fecdd
oluwarotimi1 <adebisioluwarotimiaa1@gmail.com>
2022-10-08 10:38:05 +0100

    Header Updated
    Necessary Header Element Updated

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me_4.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h2>ADEBISI OLUWAROTIMI</h2>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        
+       
+        <div className="me">
+          <img src={ME} alt='me'/>
+        </div>
+        
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+      <HeaderSocials />
+    </header>
   )
 }
 
