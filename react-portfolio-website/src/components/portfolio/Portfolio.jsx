941f70dba313f92c96419781006debc4e902e9b9
oluwarotimi1 <adebisioluwarotimiaa1@gmail.com>
2022-10-09 16:01:11 +0100

    Testimonials Updated

--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,19 +1,76 @@
 import React from 'react'
 import './portfolio.css'
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
+import img4 from '../../assets/img4.png'
+import img5 from '../../assets/img5.png'
+import img6 from '../../assets/img6.png'
 
 const Portfolio = () => {
   return (
     <section id='portfolio'> 
       <h5>My Works</h5>
-      <h2>Portfolio</h2>
+      <h2>Past Projects</h2>
 
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="http://github.com" className='btn' target='_blank'> GitHub</a>
-          <a href="#" className='btn btn-primary' target='_blank'> Live Demo</a>
+          <div className="portfolio__item-image">
+            <img src={img1} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+          <h3>25 Design Studio Created For User Centered Designs</h3>
+          <a href="https://gifted-mahavira-756053.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
         </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img2} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+            <h3>Bootcamp Website To Becoming A Web Developer</h3>
+            <a href="https://compassionate-snyder-6040d4.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
+         </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img3} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+             <h3>An E-Commerce Website For Super Value Deals</h3>
+            <a href="https://meek-tulumba-0804b4.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
+          </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img5} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+            <h3>A Single Live Filter Page Linked To A Random API</h3>
+            <a href="https://lighthearted-cobbler-7ce921.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
+          </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img4} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+            <h3>Website Built For "Organic Foods" For Online Orders</h3>
+            <a href="https://pedantic-payne-d7d724.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
+          </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img6} alt='' />
+          </div>
+          <div className="portfolio__item-cta">
+             <h3>Weather Application </h3>
+            <a href="https://super-pudding-7cb3b2.netlify.app/" className='btn btn-primary' target='_blank'> Live Demo</a>
+          </div>
+         </article>
       </div>
     </section>
   )
--- react-portfolio-website/src/components/testimonials/Testimonials.jsx
+++ react-portfolio-website/src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
 import './testimonials.css'
+import AVATAR1 from '../../assets/assets_2/avatar1.jpg'
+import AVATAR2 from '../../assets/assets_2/avatar2.jpg'
+import AVATAR3 from '../../assets/assets_2/avatar3.jpg'
+
+
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data=[
+  {
+    avatar: AVATAR1,
+    name:'Esther Paul',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, tempora nemo? Esse cum quas repudiandae, dolore quia quisquam beatae nihil minus labore, odit officiis laudantium, ratione architecto quis repellat quaerat.'
+  },
+  {
+    avatar: AVATAR2,
+    name:'Peter Atabo',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, tempora nemo? Esse cum quas repudiandae, dolore quia quisquam beatae nihil minus labore, odit officiis laudantium, ratione architecto quis repellat quaerat.'
+  },
+  {
+    avatar: AVATAR3,
+    name:'Alwawiye Kayode',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, tempora nemo? Esse cum quas repudiandae, dolore quia quisquam beatae nihil minus labore, odit officiis laudantium, ratione architecto quis repellat quaerat.'
+  }
+]
 
 const Testimonials = () => {
   return (
     <section id='testimonials'>
-      Testimonials
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container mySwiper"
+        modules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review}, index)=>{
+            return(
+              <SwiperSlide key={index} className="testimonials">
+                <div className="client__avatar">
+                  <img src={avatar} alt="AVATAR one" />
+                </div>
+                <div>
+                <h5 className="client__name">{name}</h5>
+                  <small className="client__review">
+                    {review}
+                  </small>
+                </div>
+              </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
